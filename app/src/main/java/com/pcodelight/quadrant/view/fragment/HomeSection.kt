793decf054f67552ac3ab4e752f8109c06dd58fe
Parlindungan Sinabariba <parlindungan@bukalapak.com>
2020-10-01 21:53:08 +0800

    Toast on data successfully sent

--- app/src/main/java/com/pcodelight/quadrant/view/fragment/HomeSection.kt
+++ app/src/main/java/com/pcodelight/quadrant/view/fragment/HomeSection.kt
@@ -3,11 +3,13 @@ package com.pcodelight.quadrant.view.fragment
 import android.location.Location
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.pcodelight.quadrant.R
 import com.pcodelight.quadrant.viewmodel.DashboardViewModel
 import com.pcodelight.quadrant.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_login_screen.view.*
 import kotlinx.android.synthetic.main.section_home.view.*
 
 class HomeSection : Fragment(
@@ -28,9 +30,19 @@ class HomeSection : Fragment(
                 as DashboardViewModel
 
         viewModel.apply {
-            locationData.observe(requireActivity(), Observer {
-                view.btnSendData.isEnabled = it != null
-                view.tvStatus.text = getFormattedLocation(it) ?: "No location available"
+            locationData.observe(requireActivity(), Observer { location ->
+                if (location == null) {
+                    view.btnSendData.isEnabled = false
+                    view.tvStatus.text = getString(R.string.no_location)
+                } else {
+                    view.btnSendData.isEnabled = true
+                    view.tvStatus.text = getFormattedLocation(location)
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.sent_successfully),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             })
 
             isGettingDeviceLocation.observe(requireActivity(), Observer { isRetrievingLocation ->
